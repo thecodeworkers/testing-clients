07ec9c784966c44baeb10ace847d9141edd5d3e9
Jorge Bastidas <jorgebastidas9@gmail.com>
2020-11-27 10:06:57 -0400

    tu mama fue mia gone

--- pix/pix_setting_client/business_setting_client.py
+++ pix/pix_setting_client/business_setting_client.py
@@ -341,7 +341,7 @@ class BusinessSettingClient():
 
 client = BusinessSettingClient()
 
-#print(client.get_all())
+print(client.get_all())
 #print(client.get_one_case_a())
 #print(client.get_one_case_b())
 #print(client.get_one_case_c())
